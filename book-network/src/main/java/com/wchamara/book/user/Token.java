01cff0108e0f211ae059c6cc906e48b273e41903
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-09-16 22:43:50 +0530

    Refactor Token class for user authentication and authorization

--- book-network/src/main/java/com/wchamara/book/user/Token.java
+++ book-network/src/main/java/com/wchamara/book/user/Token.java
@@ -1,4 +1,32 @@
 package com.wchamara.book.user;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Token {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String token;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(nullable = false,name = "userId")
+    private User user;
+
+
 }
