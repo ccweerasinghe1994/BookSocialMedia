eb9773b4bb0a9fac24b5cf3fb308c1741d85c455
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-09-17 08:25:52 +0530

    Add AuthenticationService for user registration

---
+++ book-network/src/main/java/com/wchamara/book/auth/AuthenticationService.java
@@ -0,0 +1,10 @@
+package com.wchamara.book.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    public void register(RegistrationRequest request) {
+
+    }
+}
