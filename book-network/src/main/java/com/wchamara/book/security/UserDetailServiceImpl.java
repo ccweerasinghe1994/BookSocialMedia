7c609efb918c5ee89406f2bcf53c71c1ce9b5739
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-09-17 08:27:04 +0530

    Add UserDetailServiceImpl class for implementing UserDetailsService interface

---
+++ book-network/src/main/java/com/wchamara/book/security/UserDetailServiceImpl.java
@@ -0,0 +1,33 @@
+package com.wchamara.book.security;
+
+import com.wchamara.book.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    /**
+     * Locates the user based on the username. In the actual implementation, the search
+     * may be case-sensitive, or case-insensitive depending on how the
+     * implementation instance is configured. In this case, the <code>UserDetails</code>
+     * object that comes back may have a username that is of a different case than what
+     * was actually requested.
+     *
+     * @param username the username identifying the user whose data is required.
+     * @return a fully populated user record (never <code>null</code>)
+     * @throws UsernameNotFoundException if the user could not be found or the user has no
+     *                                   GrantedAuthority
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
